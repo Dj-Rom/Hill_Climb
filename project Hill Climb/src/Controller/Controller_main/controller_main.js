5c3f2b3fdca6e6f31ce221f5d3828fcfd6c33efe
Dj-Rom <118632628+Dj-Rom@users.noreply.github.com>
2023-05-31 21:57:15 +0200

    Update controller_main.js

--- project Hill Climb/src/Controller/Controller_main/controller_main.js
+++ project Hill Climb/src/Controller/Controller_main/controller_main.js
@@ -1,26 +1,58 @@
 
 
-let pressedRightBool = true;
-function pressedEvent(){
-    
+import { k } from "https://dj-rom.github.io/Hill_Climb/project%20Hill%20Climb/src/model/model_main/JS/main.js";
+console.log(k)
 document.addEventListener("keydown", ()=>{
-
+event.preventDefault()
     if( event.key === 'ArrowRight'){
-   return  pressedRightBool = true;
+      k.ArrowRight = 1  
+    }  
+    if( event.key === 'ArrowLeft'){
+        k.ArrowLeft = 1
+    }  
+    if( event.key === 'ArrowUp'){
+        k.ArrowUp = 1
+    }  
+    if( event.key === 'ArrowDown'){
+        k.ArrowDown = 1
     }  
 
    
+}
+
+)
+document.addEventListener("keyup",()=>{
+    event.preventDefault()
+    if( event.key === 'ArrowRight'){
+        k.ArrowRight = 0  
+      }  
+      if( event.key === 'ArrowLeft'){
+          k.ArrowLeft = 0
+      }  
+      if( event.key === 'ArrowUp'){
+          k.ArrowUp = 0
+      }  
+      if( event.key === 'ArrowDown'){
+          k.ArrowDown = 0
+      }  
+
+})
+
+
+window.addEventListener('deviceorientation', function(event) {
+    if (event.beta > '30' ) {
+        k.ArrowLeft = 1
+    } else if (event.beta < '-30' ) {
+        k.ArrowRight = 1  
+    }
+
 });
 
-requestAnimationFrame(pressedEvent)
-return pressedRightBool
-
-}
-console.log(pressedEvent());
-
-export let  pressedRight = pressedEvent();
 
 
+    var el = document.getElementById("canvas");
+    el.addEventListener("touchstart",   ()=>{  k.ArrowUp = 1}, false);
+    el.addEventListener("touchend",  ()=>{  k.ArrowUp = 0}, false);
+    
 
-
-
+  
