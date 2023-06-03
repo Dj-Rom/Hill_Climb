14badf3d61b35c47c04e5f481621fa23946c7c10
Dj-Rom <118632628+Dj-Rom@users.noreply.github.com>
2023-06-03 22:10:46 +0200

    Update controller_main.js

--- project Hill Climb/src/Controller/Controller_main/controller_main.js
+++ project Hill Climb/src/Controller/Controller_main/controller_main.js
@@ -1,58 +1,58 @@
 
 
-import { k } from "https://dj-rom.github.io/Hill_Climb/project%20Hill%20Climb/src/model/model_main/JS/main.js";
-console.log(k)
-document.addEventListener("keydown", ()=>{
-event.preventDefault()
-    if( event.key === 'ArrowRight'){
-      k.ArrowRight = 1  
-    }  
-    if( event.key === 'ArrowLeft'){
-        k.ArrowLeft = 1
-    }  
-    if( event.key === 'ArrowUp'){
-        k.ArrowUp = 1
-    }  
-    if( event.key === 'ArrowDown'){
-        k.ArrowDown = 1
-    }  
+import { kontrole } from "https://dj-rom.github.io/Hill_Climb/project%20Hill%20Climb/src/model/model_main/JS/main.js";
 
-   
-}
-
-)
-document.addEventListener("keyup",()=>{
-    event.preventDefault()
-    if( event.key === 'ArrowRight'){
-        k.ArrowRight = 0  
-      }  
-      if( event.key === 'ArrowLeft'){
-          k.ArrowLeft = 0
-      }  
-      if( event.key === 'ArrowUp'){
-          k.ArrowUp = 0
-      }  
-      if( event.key === 'ArrowDown'){
-          k.ArrowDown = 0
-      }  
-
-})
-
-
-window.addEventListener('deviceorientation', function(event) {
-    if (event.beta > '10' ) {
-        k.ArrowRight = 1
-    } else if (event.beta < '-10' ) {
-          k.ArrowLeft = 1
+document.addEventListener("keydown", () => {
+    event.preventDefault();
+    if (event.key === "ArrowRight") {
+        kontrole.ArrowRight = 1;
     }
-
+    if (event.key === "ArrowLeft") {
+        kontrole.ArrowLeft = 1;
+    }
+    if (event.key === "ArrowUp") {
+        kontrole.ArrowUp = 1;
+    }
+    if (event.key === "ArrowDown") {
+        kontrole.ArrowDown = 1;
+    }
+});
+document.addEventListener("keyup", () => {
+    event.preventDefault();
+    if (event.key === "ArrowRight") {
+        kontrole.ArrowRight = 0;
+    }
+    if (event.key === "ArrowLeft") {
+        kontrole.ArrowLeft = 0;
+    }
+    if (event.key === "ArrowUp") {
+        kontrole.ArrowUp = 0;
+    }
+    if (event.key === "ArrowDown") {
+        kontrole.ArrowDown = 0;
+    }
 });
 
+window.addEventListener("deviceorientation", function (event) {
+    if (event.beta > "3") {
+        kontrole.ArrowRight = 1;
+    } else if (event.beta < "-3") {
+        kontrole.ArrowLeft = 1;
+    }
+});
 
-
-    const el = document.getElementById("canvas");
-    el.addEventListener("touchstart",   ()=>{  k.ArrowUp = 1}, false);
-    el.addEventListener("touchend",  ()=>{  k.ArrowUp = 0}, false);
-    
-
-  
+var el = document.getElementById("canvas");
+el.addEventListener(
+    "touchstart",
+    () => {
+        k.ArrowUp = 1;
+    },
+    false
+);
+el.addEventListener(
+    "touchend",
+    () => {
+        k.ArrowUp = 0;
+    },
+    false
+);
