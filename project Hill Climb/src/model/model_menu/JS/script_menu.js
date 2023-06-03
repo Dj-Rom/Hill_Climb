0fd4ae803f1e7d5242991010b0e0734dca758fdf
Dj-Rom <118632628+Dj-Rom@users.noreply.github.com>
2023-06-03 18:49:26 +0200

    Update script_menu.js

--- project Hill Climb/src/model/model_menu/JS/script_menu.js
+++ project Hill Climb/src/model/model_menu/JS/script_menu.js
@@ -1,15 +1,44 @@
+window.addEventListener("load",()=>{
+
+const welcom = document.createElement("div")
+welcom.id = "welcom";
+welcom.innerText = "Student project Hill Climb"
+welcom.style.opacity = 0.99
+document.body.append(welcom)
+
+let contener = document.querySelector(".contener");
+
+ let welcomGet = document.querySelector('#welcom')
+ let hw = 100
+ welcomGet.style.height = hw+"%"
+ welcomGet.style.transition = "transition: width 4s, height 4s, transform 15s;"
+ contener.style.transition = "transition: width 4s, height 2s, transform 15s;"
+ contener.style.opacity = .01
+let timer = setInterval(()=>{
+  contener.style.opacity *=1.07
+
+  welcomGet.style.opacity -=0.00899999;
+
+
+
+if(welcomGet.style.opacity <= 0.00 && contener.style.opacity > 0.90 ){playAudio(aud); clearInterval(timer); 
+}
+},20)
+
+
+
 const soundAudio = document.createElement("audio");
 soundAudio.id = "audioT";
 soundAudio.loop = "loop";
-soundAudio.autoplay = 'autoplay'
-
 document.body.append(soundAudio);
 
 let aud = document.getElementById("audioT").muted;
 aud = new Audio("https://dj-rom.github.io/Hill_Climb/project%20Hill%20Climb/src/view/music/menu.mp3");
 
-function playAudio() {
-  aud.play();
+
+function playAudio(elem) {
+  elem.play()
+  
 }
 
 function pauseAudio() {
@@ -23,12 +52,27 @@ off.style.display = "block";
 
 function soundControl() {
   requestAnimationFrame(soundControl);
-
+  
   if (off.style.display === "block") {
-    playAudio();
+    playAudio(aud);
   }
   if (on.style.display === "block") {
     pauseAudio();
   }
 }
 soundControl();
+
+
+const bodyM = document.querySelector('body')
+function openFullscreen(elem) {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
+openFullscreen(bodyM)
+
+})
