080caf7d6a22c0ebe9a9c9bde24ddb055fc214bc
Dj-Rom <118632628+Dj-Rom@users.noreply.github.com>
2023-06-03 12:50:26 +0200

    Update script_menu.js

--- project Hill Climb/src/model/model_menu/JS/script_menu.js
+++ project Hill Climb/src/model/model_menu/JS/script_menu.js
@@ -7,8 +7,6 @@ let audioT = document.getElementById("audioT");
 audioT = new Audio("https://dj-rom.github.io/Hill_Climb/project%20Hill%20Climb/src/view/music/menu.mp3")
 
 
-
-
 function playAudio(elem) { 
   elem.play(); 
 } 
