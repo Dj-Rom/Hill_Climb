b27dc0b09f14725b7ec262a0a951f7b51650a9fc
Dj-Rom <118632628+Dj-Rom@users.noreply.github.com>
2023-06-03 15:19:32 +0200

    Update script_menu.js

--- project Hill Climb/src/model/model_menu/JS/script_menu.js
+++ project Hill Climb/src/model/model_menu/JS/script_menu.js
@@ -1,20 +1,34 @@
-const soundAudio = document.createElement('audio');
-soundAudio.id = "audioT"
-soundAudio.loop = "loop"
+const soundAudio = document.createElement("audio");
+soundAudio.id = "audioT";
+soundAudio.loop = "loop";
+document.body.append(soundAudio);
 
-document.body.append(soundAudio)
-let aud = document.getElementById("audioT"); 
-aud = new Audio("https://dj-rom.github.io/Hill_Climb/project%20Hill%20Climb/src/view/music/menu.mp3")
+let aud = document.getElementById("audioT");
+aud = new Audio(
+  "https://dj-rom.github.io/Hill_Climb/project%20Hill%20Climb/src/view/music/menu.mp3"
+);
 
+function playAudio() {
+  aud.play();
+}
 
-function playAudio() { 
-    aud.play(); 
-  } 
-  
-  function pauseAudio() { 
-    aud.pause(); 
-  } 
+function pauseAudio() {
+  aud.pause();
+}
 
+const on = document.getElementById("soundOn");
+const off = document.getElementById("soundOff");
+on.style.display = "none";
+off.style.display = "block";
 
-  playAudio() 
+function soundControl() {
+  requestAnimationFrame(soundControl);
 
+  if (off.style.display === "block") {
+    playAudio();
+  }
+  if (on.style.display === "block") {
+    pauseAudio();
+  }
+}
+soundControl();
