fb65c5b70201a31eeb4e4f3bdf248cf5e3e34e49
Dj-Rom <118632628+Dj-Rom@users.noreply.github.com>
2023-06-03 12:46:56 +0200

    Update script_menu.js

--- project Hill Climb/src/model/model_menu/JS/script_menu.js
+++ project Hill Climb/src/model/model_menu/JS/script_menu.js
@@ -1,12 +1,11 @@
 const soundAudio = document.createElement('audio');
 soundAudio.id = "audioT"
 soundAudio.loop = "loop"
+soundAudio.autoplay = "autoplay"
 document.body.append(soundAudio)
 let audioT = document.getElementById("audioT"); 
 audioT = new Audio("https://dj-rom.github.io/Hill_Climb/project%20Hill%20Climb/src/view/music/menu.mp3")
 
-
- audioT.play();
 
 
 
@@ -17,4 +16,4 @@ function playAudio(elem) {
 function pauseAudio(elem) { 
   elem.pause(); 
 } 
-playAudio(audioT)
+
