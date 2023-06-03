c2ec4b683e83ff73829854534e06aaabff262f4c
Dj-Rom <118632628+Dj-Rom@users.noreply.github.com>
2023-06-03 15:51:44 +0200

    Update script_menu.js

--- project Hill Climb/src/model/model_menu/JS/script_menu.js
+++ project Hill Climb/src/model/model_menu/JS/script_menu.js
@@ -1,7 +1,7 @@
 const soundAudio = document.createElement("audio");
 soundAudio.id = "audioT";
 soundAudio.loop = "loop";
-soundAudio.soundAudio = 'soundAudio'
+soundAudio.autoplay = 'autoplay'
 soundAudio.controls = 'controls'
 document.body.append(soundAudio);
 
