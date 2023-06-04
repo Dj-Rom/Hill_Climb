e45b18cbde2280d0baa749a531259442489129cb
Dj-Rom <118632628+Dj-Rom@users.noreply.github.com>
2023-06-04 15:17:03 +0200

    Update controller_main.js

--- project Hill Climb/src/Controller/Controller_main/controller_main.js
+++ project Hill Climb/src/Controller/Controller_main/controller_main.js
@@ -1,5 +1,6 @@
 import { kontrole } from "https://dj-rom.github.io/Hill_Climb/project%20Hill%20Climb/src/model/model_main/JS/main.js";
 
+
 console.log(kontrole);
 document.addEventListener("keydown", () => {
     event.preventDefault();
@@ -32,27 +33,45 @@ document.addEventListener("keyup", () => {
     }
 });
 
-window.addEventListener("deviceorientation", function (event) {
-    if (event.beta > "3") {
-        kontrole.ArrowRight = 1;
-    } else if (event.beta < "-3") {
-        kontrole.ArrowLeft = 1;
-    }
-});
+// window.addEventListener("deviceorientation", function (event) {
+//     if (event.beta > "3") {
+//         kontrole.ArrowRight = 1;
+//     } else if (event.beta < "-3") {
+//         kontrole.ArrowLeft = 1;
+//     }
+// });
 
+const imgGaz = document.getElementById("imgGaz");
 
-document.addEventListener("touchstart", () => {
+imgGaz.addEventListener("touchstart", () => {
         
-       return kontrole.ArrowUp = 1, console.log(kontrole);
+       return kontrole.ArrowUp = 1
     }
 
 );
-document.addEventListener("touchend",
+imgGaz.addEventListener("touchend",
     () => {
         kontrole.ArrowUp = 0;
     }
 );
+
+const imgbr = document.getElementById("imgbr");
+
+imgbr.addEventListener("touchstart", () => {
+        
+       return kontrole.ArrowDown = 1
+    }
+
+);
+imgbr.addEventListener("touchend",
+    () => {
+        kontrole.ArrowDown = 0;
+    }
+);
+
+
 window.navigator.vibrate(200)
+
 
 
 
