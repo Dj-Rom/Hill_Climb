264084102e8667a764678974da3c9c77512706ba
Dj-Rom <118632628+Dj-Rom@users.noreply.github.com>
2023-06-04 15:22:05 +0200

    Update controller_main.js

--- project Hill Climb/src/Controller/Controller_main/controller_main.js
+++ project Hill Climb/src/Controller/Controller_main/controller_main.js
@@ -34,6 +34,7 @@ document.addEventListener("keyup", () => {
 });
 
 // window.addEventListener("deviceorientation", function (event) {
+event.preventDefault()
 //     if (event.beta > "3") {
 //         kontrole.ArrowRight = 1;
 //     } else if (event.beta < "-3") {
@@ -44,27 +45,28 @@ document.addEventListener("keyup", () => {
 const imgGaz = document.getElementById("imgGaz");
 
 imgGaz.addEventListener("touchstart", () => {
+    event.preventDefault()
         
        return kontrole.ArrowUp = 1
     }
 
 );
 imgGaz.addEventListener("touchend",
-    () => {
+    () => {event.preventDefault()
         kontrole.ArrowUp = 0;
     }
 );
 
 const imgbr = document.getElementById("imgbr");
 
-imgbr.addEventListener("touchstart", () => {
+imgbr.addEventListener("touchstart", () => {event.preventDefault()
         
        return kontrole.ArrowDown = 1
     }
 
 );
 imgbr.addEventListener("touchend",
-    () => {
+    () => {event.preventDefault()
         kontrole.ArrowDown = 0;
     }
 );
