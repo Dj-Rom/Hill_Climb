f5b332fee41bf3ab4eb07e59339e6e61fdf9ac49
Dj-Rom <118632628+Dj-Rom@users.noreply.github.com>
2023-06-06 21:00:13 +0200

    Update controller_main.js

--- project Hill Climb/src/Controller/Controller_main/controller_main.js
+++ project Hill Climb/src/Controller/Controller_main/controller_main.js
@@ -1,33 +1,37 @@
 import { kontrole } from "https://dj-rom.github.io/Hill_Climb/project%20Hill%20Climb/src/model/model_main/JS/main.js";
-
+let data = JSON.parse(localStorage["HillClimb"])
+//      data.forwardKey
+//  data.backKey
+// data.B_forwardKey
+// data.B_BackKey
 
 document.addEventListener("keydown", () => {
     event.preventDefault();
-    if (event.key === "ArrowRight") {
+    if (event.key === data.B_forwardKey) {
         kontrole.ArrowRight = 1;
     }
-    if (event.key === "ArrowLeft") {
+    if (event.key === data.B_BackKey) {
         kontrole.ArrowLeft = 1;
     }
-    if (event.key === "ArrowUp") {
+    if (event.key === data.forwardKey) {
         kontrole.ArrowUp = 1;
     }
-    if (event.key === "ArrowDown") {
+    if (event.key === data.backKey) {
         kontrole.ArrowDown = 1;
     }
 });
 document.addEventListener("keyup", () => {
     event.preventDefault();
-    if (event.key === "ArrowRight") {
+    if (event.key === data.B_forwardKey) {
         kontrole.ArrowRight = 0;
     }
-    if (event.key === "ArrowLeft") {
+    if (event.key === data.B_BackKey) {
         kontrole.ArrowLeft = 0;
     }
-    if (event.key === "ArrowUp") {
+    if (event.key === data.forwardKey) {
         kontrole.ArrowUp = 0;
     }
-    if (event.key === "ArrowDown") {
+    if (event.key === data.backKey) {
         kontrole.ArrowDown = 0;
     }
 });
@@ -35,30 +39,32 @@ document.addEventListener("keyup", () => {
 let timer = null;
 // На первое срабатывание реагируем всегда
 let oldAngle = 1000;
-window.addEventListener('deviceorientation', function(event) {
-     event.preventDefault();
-  let angle = event.beta;
-  let delta = Math.abs(angle - oldAngle);
-  oldAngle = angle;
-  if (delta < 2)  // Если уголо наклона изменился не сильно
-    return;  // выходим
-  if (timer)
-    clearTimeout(timer);
-  timer = setTimeout(function() {
-    timer = null;
-    if (angle >10) {
-       kontrole.ArrowRight = 1;
-        kontrole.ArrowLeft = 0
-    } else if (angle < -10) {
-        kontrole.ArrowRight = 0;
-      kontrole.ArrowLeft = 1;
-    }else if (angle < 10 && angle > -10 ){
-        kontrole.ArrowRight = 0;
-      kontrole.ArrowLeft = 0;
-  }
-      else if (angle > 10 && angle < -10 ){
-        kontrole.ArrowRight = 0;
-      kontrole.ArrowLeft = 0;}}, 16);
+window.addEventListener('deviceorientation', function (event) {
+    event.preventDefault();
+    let angle = event.beta;
+    let delta = Math.abs(angle - oldAngle);
+    oldAngle = angle;
+    if (delta < 2)  // Если уголо наклона изменился не сильно
+        return;  // выходим
+    if (timer)
+        clearTimeout(timer);
+    timer = setTimeout(function () {
+        timer = null;
+        if (angle > 10) {
+            kontrole.ArrowRight = 1;
+            kontrole.ArrowLeft = 0
+        } else if (angle < -10) {
+            kontrole.ArrowRight = 0;
+            kontrole.ArrowLeft = 1;
+        } else if (angle < 10 && angle > -10) {
+            kontrole.ArrowRight = 0;
+            kontrole.ArrowLeft = 0;
+        }
+        else if (angle > 10 && angle < -10) {
+            kontrole.ArrowRight = 0;
+            kontrole.ArrowLeft = 0;
+        }
+    }, 16);
 });
 
 
@@ -66,43 +72,45 @@ const imgGaz = document.getElementById("imgGaz");
 
 imgGaz.addEventListener("touchstart", () => {
     event.preventDefault()
-        window.navigator.vibrate(200)
-       return kontrole.ArrowUp = 1
-    },false
+    window.navigator.vibrate(200)
+    return kontrole.ArrowUp = 1
+}, false
 );
 imgGaz.addEventListener("touchend",
-    () => {event.preventDefault()
+    () => {
+        event.preventDefault()
         kontrole.ArrowUp = 0;
-    },false
+    }, false
 );
 imgGaz.addEventListener("click", () => {
     event.preventDefault()
-        window.navigator.vibrate(200)
-       return kontrole.ArrowUp = 1
-    },false
+    window.navigator.vibrate(200)
+    return kontrole.ArrowUp = 1
+}, false
 
 );
-
-
 
 const imgbr = document.getElementById("imgbr");
 
-imgbr.addEventListener("touchstart", () => {event.preventDefault()
-        window.navigator.vibrate(200)
-       return kontrole.ArrowDown = 1
-    },false
+imgbr.addEventListener("touchstart", () => {
+    event.preventDefault()
+    window.navigator.vibrate(200)
+    return kontrole.ArrowDown = 1
+}, false
 
 );
 imgbr.addEventListener("touchend",
-    () => {event.preventDefault()
+    () => {
+        event.preventDefault()
         kontrole.ArrowDown = 0;
-    },false
+    }, false
 );
 
-imgbr.addEventListener("click", () => {event.preventDefault()
-        window.navigator.vibrate(200)
-       return kontrole.ArrowDown = 1
-    },false
+imgbr.addEventListener("click", () => {
+    event.preventDefault()
+    window.navigator.vibrate(200)
+    return kontrole.ArrowDown = 1
+}, false
 
 );
 
