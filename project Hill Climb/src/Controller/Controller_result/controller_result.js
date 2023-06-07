858aa5469d38614ddf08ede65722ad905a76667a
Dj-Rom <118632628+Dj-Rom@users.noreply.github.com>
2023-06-07 14:25:25 +0200

    Update controller_result.js

--- project Hill Climb/src/Controller/Controller_result/controller_result.js
+++ project Hill Climb/src/Controller/Controller_result/controller_result.js
@@ -1,4 +1,4 @@
-const btnR = document.getElementById('btnR')
+// const btnR = document.getElementById('btnR')
 btnR.addEventListener('click',()=>{
     location = "https://dj-rom.github.io/Hill_Climb/project%20Hill%20Climb/src/view/view_menu/menu.html" 
 })
