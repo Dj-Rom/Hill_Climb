251b5772b526b88a2def06e0255699489719a274
Dj-Rom <118632628+Dj-Rom@users.noreply.github.com>
2023-06-07 14:10:04 +0200

    Add files via upload

---
+++ project Hill Climb/src/Controller/Controller_result/controller_result.js
@@ -0,0 +1,4 @@
+const btnR = document.getElementById('btnR')
+btnR.addEventListener('click',()=>{
+    location = "https://dj-rom.github.io/Hill_Climb/project%20Hill%20Climb/src/view/view_menu/menu.html" 
+})
