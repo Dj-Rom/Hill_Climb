d4696be5be1b40f109f06067c9cc4d2047e15341
Dj-Rom <118632628+Dj-Rom@users.noreply.github.com>
2023-06-08 08:39:06 +0200

    Add files via upload

---
+++ project Hill Climb/src/main.js
@@ -0,0 +1,33 @@
+document.addEventListener('click', eo =>{
+    eo = eo || window.event;
+    target = eo.target || eo.srcElement
+    eo.preventDefault();
+    console.log(eo.target.tagName);
+    if(eo.target.tagName == 'BUTTON'){
+        console.log(eo.target.tagName);
+        route(eo);
+    }
+    
+})
+
+const route = (eo)=>{
+    eo = eo || window.event
+    console.log(eo.target.href);
+    history.pushState({}, '', eo.target.href);
+    handleLocation();
+}
+const routers = {
+    'https://dj-rom.github.io/Hill_Climb/project%20Hill%20Climb/src/view/view_menu/menu.html': 'https://dj-rom.github.io/Hill_Climb/project%20Hill%20Climb/src/view/view_menu/menu.html',
+    '/New game': 'https://dj-rom.github.io/Hill_Climb/project%20Hill%20Climb/src/view/view_main/main_game_start'
+}
+const handleLocation = async ()=>{
+    const path = location.pathname;
+    const html = await fetch(routers[path]).then((data) => data.text());
+    document.querySelector('.contener').innerHTML = html;
+
+
+}
+
+window.onpopstate = handleLocation;
+window.route = route;
+handleLocation()
