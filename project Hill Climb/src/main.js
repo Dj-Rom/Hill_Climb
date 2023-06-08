4317b7ce393ee71ffe84d101fe395fb01c15fa19
Dj-Rom <118632628+Dj-Rom@users.noreply.github.com>
2023-06-08 10:10:28 +0200

    Update main.js

--- project Hill Climb/src/main.js
+++ project Hill Climb/src/main.js
@@ -16,7 +16,7 @@ const route = (eo)=>{
     handleLocation();
 }
 const routers = {
-    '/': 'https://dj-rom.github.io/Hill_Climb/project%20Hill%20Climb/src/view/view_menu/menu.html',
+    '/Hill_Climb/project%20Hill%20Climb/src/': 'https://dj-rom.github.io/Hill_Climb/project%20Hill%20Climb/src/view/view_menu/menu.html',
     '/New game': 'https://dj-rom.github.io/Hill_Climb/project%20Hill%20Climb/src/view/view_main/main_game_start'
 }
 const handleLocation = async ()=>{
