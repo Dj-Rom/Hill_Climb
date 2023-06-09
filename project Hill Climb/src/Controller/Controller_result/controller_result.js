052ce11812148461883d9a9335907c07d1ab575d
Aleh Kapusta <118632628+Dj-Rom@users.noreply.github.com>
2023-06-09 23:08:35 +0200

    Update controller_result.js

--- project Hill Climb/src/Controller/Controller_result/controller_result.js
+++ project Hill Climb/src/Controller/Controller_result/controller_result.js
@@ -1,3 +1,3 @@
-document.getElementById('menuR').addEventListener('click',()=>{
-    location = "https://dj-rom.github.io/Hill_Climb/project%20Hill%20Climb/src/view/view_menu/menu.html" 
-})
+// document.getElementById('menuR').addEventListener('click',()=>{
+//     location = "https://dj-rom.github.io/Hill_Climb/project%20Hill%20Climb/src/view/view_menu/menu.html" 
+// })
