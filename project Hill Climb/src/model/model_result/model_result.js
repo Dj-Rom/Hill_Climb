20ed01b0e21bae00525d1060f33a88ea0396a262
Aleh Kapusta <118632628+Dj-Rom@users.noreply.github.com>
2023-06-09 20:44:55 +0200

    Update model_result.js

--- project Hill Climb/src/model/model_result/model_result.js
+++ project Hill Climb/src/model/model_result/model_result.js
@@ -1,6 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
-    alert("DOM готов!");
-  });
+
 const tblRecord = document.getElementById("result_tbl");
 const cont = document.querySelector('.contener')
 const dataRecord= {"record":[{nameRecord: "Anton", score: 2000},{nameRecord: "Anton", score: 2000},{nameRecord: "Anton", score: 2000},{nameRecord: "Anton", score: 2000},{nameRecord: "Anton", score: 2000},{nameRecord: "Anton", score: 2000},{nameRecord: "Anton", score: 2000},{nameRecord: "Anton", score: 2000},{nameRecord: "Anton", score: 2000},{nameRecord: "Anton", score: 2000},{nameRecord: "Anton", score: 2000},{nameRecord: "Anton", score: 2000},{nameRecord: "Anton", score: 2000},{nameRecord: "Anton", score: 2000},{nameRecord: "Anton", score: 200450},{nameRecord: "Anton", score: 20006},{nameRecord: "Anton", score: 22000},{nameRecord: "Anton", score: 200},{nameRecord: "Anton", score: 2022200},{nameRecord: "Anton", score: 20000},{nameRecord: "Anton", score: 2000},{nameRecord: "Anton", score: 20030},{nameRecord: "Anton", score: 20300},]} 
