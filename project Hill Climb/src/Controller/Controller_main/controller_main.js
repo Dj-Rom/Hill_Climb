4668d3c27c6b25cddbbcec2ff994f816367d5d0c
Aleh Kapusta <118632628+Dj-Rom@users.noreply.github.com>
2023-06-10 03:34:48 +0200

    Update controller_main.js

--- project Hill Climb/src/Controller/Controller_main/controller_main.js
+++ project Hill Climb/src/Controller/Controller_main/controller_main.js
@@ -1,5 +1,6 @@
 import { kontrole } from "https://dj-rom.github.io/Hill_Climb/project%20Hill%20Climb/src/model/model_main/JS/main.js";
 let data = JSON.parse(localStorage["HillClimb"])
+console.log(data)
 if(!data|| data === ""){
     data.forwardKey = 'ArrowUp',
     data.backKey = 'ArrowDown',
