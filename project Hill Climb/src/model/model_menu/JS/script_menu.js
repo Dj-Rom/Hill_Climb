1a1a658b9ffe2903372777d7ade8335fdf9c4951
Aleh Kapusta <118632628+Dj-Rom@users.noreply.github.com>
2023-06-10 08:22:59 +0200

    Update script_menu.js

--- project Hill Climb/src/model/model_menu/JS/script_menu.js
+++ project Hill Climb/src/model/model_menu/JS/script_menu.js
@@ -43,17 +43,6 @@ function soundControl() {
   }
 }
 soundControl();
-    let html = document.documentElement;
 
-function fullScreen(element) {
-  if(element.requestFullscreen) {
-    element.requestFullscreen();
-  } else if(element.webkitrequestFullscreen) {
-    element.webkitRequestFullscreen();
-  } else if(element.mozRequestFullscreen) {
-    element.mozRequestFullScreen();
-  }
-}
-fullScreen(html);
 
 })
