6074edd8c39cf2bbcebbe8958d09167fdc5fd3c6
Aleh Kapusta <118632628+Dj-Rom@users.noreply.github.com>
2023-06-12 09:16:05 +0200

    Update model_result.js

--- project Hill Climb/src/model/model_result/model_result.js
+++ project Hill Climb/src/model/model_result/model_result.js
@@ -1,42 +1,79 @@
 
-import { storeInfo } from "https://dj-rom.github.io/Hill_Climb/project%20Hill%20Climb/src/model/ajax.js";
+
 import { dataGetRecords } from "https://dj-rom.github.io/Hill_Climb/project%20Hill%20Climb/src/model/ajax.js";
-import { resultGetUserName } from "https://dj-rom.github.io/Hill_Climb/project%20Hill%20Climb/src/model/model_main/JS/main.js";
-import { resultGetUserResult } from "https://dj-rom.github.io/Hill_Climb/project%20Hill%20Climb/src/model/model_main/JS/main.js";
-
-
-
-
-
+import { storeInfo } from "https://dj-rom.github.io/Hill_Climb/project%20Hill%20Climb/src/model/ajax.js";
+import { read } from "https://dj-rom.github.io/Hill_Climb/project%20Hill%20Climb/src/model/ajax.js";
 const tblRecord = document.getElementById("result_tbl");
-const cont = document.querySelector('.contener')
-let dataRecordPred = {nameRecord: resultGetUserName, score: resultGetUserResult}
-
-export let dataRecord = {"record":[{nameRecord: "Anton", score: 20300},]} 
-dataRecord.record.push(dataRecordPred);
-
+const cont = document.querySelector('.contener');
 let newData = [];
-dataRecord.record.sort(function(a, b) {
-    return  b.score - a.score  ;
-});
-for(let i=0; i<=dataRecord.record.length;i++){
-    if(i <=10){       
-newData.push({i:dataRecord.record[i]})
+const btnR = document.createElement("button");
+btnR.id = "menuR";
+btnR.innerHTML = "MENU";
+cont.append(btnR);
+window.onload = Read();
+function Read() {
+  let dataPass;
+  let ajaxHandlerScript = "https://fe.it-academy.by/AjaxStringStorage2.php";
+  dataPass = Math.random();
+  const stringName = 'CHK123';
+  $.ajax({
+    url: ajaxHandlerScript,
+    type: 'POST',
+    cache: false,
+    dataType: 'json',
+    data: {
+      f: 'LOCKGET',
+      n: 'CHK123',
+      p: dataPass
+    },
+    success: restoreInfo1,
+    error: errorHandler
+  });
+  function restoreInfo1() {
+    $.ajax({
+      url: ajaxHandlerScript,
+      type: 'POST',
+      cache: false,
+      dataType: 'json',
+      data: {
+        f: 'READ',
+        n: 'CHK123'
+      },
+      success: readReady,
+      error: errorHandler
+    });
+  }
+  function readReady(callresult) {
+    if (callresult.error != undefined) alert(callresult.error);else if (callresult.result != "") {
+      const info = JSON.parse(callresult.result);
+      let dataRecordTable;
+      dataRecordTable = info.record;
+      dataRecordTable = dataRecordTable.filter(item => item);
+      dataRecordTable.sort(function (a, b) {
+        return b.score - a.score;
+      });
+      for (let i = 0; i <= dataRecordTable.length; i++) {
+        if (i <= dataRecordTable.length) {
+          newData.push(dataRecordTable[i]);
+          delete newData[newData.indexOf(undefined)];
+        } else if (i > 10) {
+          break;
+        }
+      }
+      for (let i = 0; i < newData.length - 2;) {
+        let pos = document.createElement("tr");
+        pos.innerHTML = `<tr >   <th>${[i + 1]}</th><th>${newData[i].nameRecord}</th><th>${newData[i].score}</th></tr>`;
+        i++;
+        tblRecord.append(pos);
+      }
+      return dataRecordTable;
+    }
+  }
+  function errorHandler(jqXHR, statusStr, errorStr) {
+    alert(statusStr + ' ' + errorStr);
+  }
+  document.addEventListener('click', () => {
+    location = "https://dj-rom.github.io/Hill_Climb/project%20Hill%20Climb/src/view/view_menu/menu.html";
+    Read();
+  });
 }
-else if(i>10){break}
-}
-for(let i = 1; i <= 10; ){
-    let pos = document.createElement("tr");
-    pos.innerHTML=  `<tr >   <th>${[i]}</th><th>${newData[JSON.stringify(i)]}</th><th>${newData[JSON.stringify(i)]}</th></tr>`
-    i++
-    tblRecord.append(pos)
-}
-const btnR = document.createElement("button")
-btnR.id = "menuR"
-btnR.innerHTML= "MENU"
-cont.append(btnR)
-document.getElementById('menuR').addEventListener('click',()=>{
-    // location = "https://dj-rom.github.io/Hill_Climb/project%20Hill%20Climb/src/view/view_menu/menu.html" 
-    storeInfo()
- console.log(dataGetRecords);
-})
