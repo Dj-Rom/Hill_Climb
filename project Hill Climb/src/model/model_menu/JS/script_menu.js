abf1c298402a562ed0674e4c8d648b9299947d1b
Aleh <alehkapusta@gmail.com>
2023-06-12 10:42:44 +0200

    Update script_menu.js

--- project Hill Climb/src/model/model_menu/JS/script_menu.js
+++ project Hill Climb/src/model/model_menu/JS/script_menu.js
@@ -1,4 +1,6 @@
 window.addEventListener("load", () => {
+  let passForDB = Math.random*0.1
+  localStorage["PassForDB"] = localStorage["PassForDB"] ? localStorage["PassForDB"] : localStorage["PassForDB"] = passForDB;
   const welcom = document.createElement("div");
   welcom.id = "welcom";
   welcom.innerText = "Student project Hill Climb";
