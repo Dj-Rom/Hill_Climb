5104a7a86255481850798322ed21463c5022f2b9
Aleh <alehkapusta@gmail.com>
2023-06-13 12:06:35 +0200

    Update ajax.js

--- project Hill Climb/src/model/ajax.js
+++ project Hill Climb/src/model/ajax.js
@@ -1,13 +1,10 @@
 import { dataRecordPred } from "https://dj-rom.github.io/Hill_Climb/project%20Hill%20Climb/src/model/model_main/JS/main.js";
 export let dataGetRecords;
 const ajaxHandlerScript = "https://fe.it-academy.by/AjaxStringStorage2.php";
-let passForDB = Math.random()
-  localStorage["PassForDB"] = localStorage["PassForDB"] ? localStorage["PassForDB"] : localStorage["PassForDB"] = JSON.stringify(passForDB);
-
 let updatePassword;
 const stringName = 'CHK123';
 export function storeInfo() {
-  updatePassword = JSON.parse(localStorage["PassForDB"])
+  updatePassword = Math.random();
   $.ajax({
     url: ajaxHandlerScript,
     type: 'POST',
@@ -49,9 +46,74 @@ function lockGetReady(callresult) {
 }
 function updateReady(callresult) {
   if (callresult.error != undefined) alert(callresult.error);
-  lockGetReady();
+  storeInfo();
 }
-
+function restoreInfo() {
+  $.ajax({
+    url: ajaxHandlerScript,
+    type: 'POST',
+    cache: false,
+    dataType: 'json',
+    data: {
+      f: 'READ',
+      n: stringName
+    },
+    success: readReady,
+    error: errorHandler
+  });
+}
+function readReady(callresult) {
+  if (callresult.error != undefined) {
+    alert(callresult.error);
+  } else if (callresult.result != "") {
+    const info = JSON.parse(callresult.result);
+    dataGetRecords = info.record;
+    return dataGetRecords;
+  }
+}
 function errorHandler(jqXHR, statusStr, errorStr) {
   alert(statusStr + ' ' + errorStr);
+}
+restoreInfo();
+export function read() {
+  $.ajax({
+    url: ajaxHandlerScript,
+    type: 'POST',
+    cache: false,
+    dataType: 'json',
+    data: {
+      f: 'LOCKGET',
+      n: stringName,
+      p: updatePassword
+    },
+    success: restoreInfo,
+    error: errorHandler
+  });
+  function restoreInfo() {
+    $.ajax({
+      url: ajaxHandlerScript,
+      type: 'POST',
+      cache: false,
+      dataType: 'json',
+      data: {
+        f: 'READ',
+        n: stringName
+      },
+      success: readReady,
+      error: errorHandler
+    });
+  }
+  function readReady(callresult) {
+    if (callresult.error != undefined) {
+      alert(callresult.error);
+      console.log('3');
+    } else if (callresult.result != "") {
+      const info = JSON.parse(callresult.result);
+      dataGetRecords = info.record;
+      return dataGetRecords;
+    }
+  }
+  function errorHandler(jqXHR, statusStr, errorStr) {
+    alert(statusStr + ' ' + errorStr);
+  }
 }
