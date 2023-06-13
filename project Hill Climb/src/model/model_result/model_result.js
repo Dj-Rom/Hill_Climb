30553ade80cfbff6743e6d3fc5422dbcdebd307b
Aleh <alehkapusta@gmail.com>
2023-06-13 22:34:46 +0200

    set

--- project Hill Climb/src/model/model_result/model_result.js
+++ project Hill Climb/src/model/model_result/model_result.js
@@ -1,9 +1,5 @@
 
 
-// import { dataGetRecords } from "https://dj-rom.github.io/Hill_Climb/project%20Hill%20Climb/src/model/ajax.js";
-
-
-// import { read } from "https://dj-rom.github.io/Hill_Climb/project%20Hill%20Climb/src/model/ajax.js";
 let passForDB = Math.random()
   localStorage["PassForDB"] = localStorage["PassForDB"] ? localStorage["PassForDB"] : localStorage["PassForDB"] = JSON.stringify(passForDB);
   let dataPass = JSON.parse(localStorage["PassForDB"])
