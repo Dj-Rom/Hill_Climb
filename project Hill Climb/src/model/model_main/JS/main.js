3ba02bb9ee8e841fde9b41ea9c7179f0ab467e25
Aleh <alehkapusta@gmail.com>
2023-06-13 22:29:34 +0200

    Update main.js

--- project Hill Climb/src/model/model_main/JS/main.js
+++ project Hill Climb/src/model/model_main/JS/main.js
@@ -292,7 +292,7 @@ function game() {
 
   gameOveSsvg.style.opacity  = 0.97 ;
  
-  
+  setTimeout(()=>{
 
     localStorage["saveGamePos"]='0'
     localStorage["HillClimbUser"] = localStorage["HillClimbUser"] ? localStorage["HillClimbUser"] : JSON.stringify({});
@@ -314,6 +314,7 @@ function game() {
       location = "https://dj-rom.github.io/Hill_Climb/project%20Hill%20Climb/src/view/view_menu/menu.html ";
     
       cancelAnimationFrame(RAF)
+    },500)
   }
   function musicFonAndGaz() {
     if (kontrole.ArrowUp === 1) {
