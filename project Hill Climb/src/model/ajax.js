71a9baec4c342dc852c8f3823bea455f82fd33dd
Aleh <alehkapusta@gmail.com>
2023-06-13 11:57:11 +0200

    Update ajax.js

--- project Hill Climb/src/model/ajax.js
+++ project Hill Climb/src/model/ajax.js
@@ -1,27 +1,13 @@
-
-
 import { dataRecordPred } from "https://dj-rom.github.io/Hill_Climb/project%20Hill%20Climb/src/model/model_main/JS/main.js";
-
-const head = document.getElementsByTagName('head')[0];
-const script = document.createElement('script');
-script.type = 'text/javascript';
-script.src = "https://fe.it-academy.by/JQ/jquery.js";
-head.appendChild(script);
 export let dataGetRecords;
 const ajaxHandlerScript = "https://fe.it-academy.by/AjaxStringStorage2.php";
-let passForDB = Math.random();
-
-
-
-
-
-
+let passForDB = Math.random()
   localStorage["PassForDB"] = localStorage["PassForDB"] ? localStorage["PassForDB"] : localStorage["PassForDB"] = JSON.stringify(passForDB);
 
-let updatePassword = JSON.parse(localStorage["PassForDB"])
+let updatePassword;
 const stringName = 'CHK123';
 export function storeInfo() {
-  
+  updatePassword = JSON.parse(localStorage["PassForDB"])
   $.ajax({
     url: ajaxHandlerScript,
     type: 'POST',
@@ -29,7 +15,7 @@ export function storeInfo() {
     dataType: 'json',
     data: {
       f: 'LOCKGET',
-      n: 'CHK123',
+      n: stringName,
       p: updatePassword
     },
     success: lockGetReady,
@@ -37,12 +23,12 @@ export function storeInfo() {
   });
 }
 function lockGetReady(callresult) {
-  if (callresult.error != undefined) alert(callresult.error);
-  else {
-
-    let info={};
-
-    info.record.push(dataRecordPred);
+  if (callresult.error != undefined) alert(callresult.error);else {
+    // нам всё равно, что было прочитано -
+    // всё равно перезаписываем
+    let info = new Object();
+    let record = new Array();
+    record.push(dataRecordPred);
     record.push(dataGetRecords);
     info.record = record.flat();
     $.ajax({
@@ -63,75 +49,6 @@ function lockGetReady(callresult) {
 }
 function updateReady(callresult) {
   if (callresult.error != undefined) alert(callresult.error);
-  // lockGetReady();
+  lockGetReady();
 }
-function restoreInfo() {
-  $.ajax({
-    url: ajaxHandlerScript,
-    type: 'POST',
-    cache: false,
-    dataType: 'json',
-    data: {
-      f: 'READ',
-      n: stringName
-    },
-    success: readReady,
-    error: errorHandler
-  });
-}
-function readReady(callresult) {
-  if (callresult.error != undefined) {
-    alert(callresult.error);
-  } else if (callresult.result != "") {
-    const info = JSON.parse(callresult.result);
-    dataGetRecords = info.record;
-    return dataGetRecords;
-  }
-}
-function errorHandler(jqXHR, statusStr, errorStr) {
-  alert(statusStr + ' ' + errorStr);
 
-restoreInfo();}
-export function read() {
-  $.ajax({
-    url: ajaxHandlerScript,
-    type: 'POST',
-    cache: false,
-    dataType: 'json',
-    data: {
-      f: 'LOCKGET',
-      n: stringName,
-      p: updatePassword
-    },
-    success: restoreInfo,
-    error: errorHandler
-  });
-  function restoreInfo() {
-    $.ajax({
-      url: ajaxHandlerScript,
-      type: 'POST',
-      cache: false,
-      dataType: 'json',
-      data: {
-        f: 'READ',
-        n: stringName
-      },
-      success: readReady,
-      error: errorHandler
-    });
-  }
-  function readReady(callresult) {
-    if (callresult.error != undefined) {
-      alert(callresult.error);
-      console.log('3');
-    } else if (callresult.result != "") {
-      const info = JSON.parse(callresult.result);
-      dataGetRecords = info.record;
-      return dataGetRecords;
-    }
-  }
-  function errorHandler(jqXHR, statusStr, errorStr) {
-    alert(statusStr + ' ' + errorStr);
-    
-  }
-}
