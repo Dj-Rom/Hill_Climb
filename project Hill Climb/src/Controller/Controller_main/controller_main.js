ac922fd6f7acb75444a2958b708f47a6645c220a
Aleh <alehkapusta@gmail.com>
2023-06-13 14:32:24 +0200

    Update controller_main.js

--- project Hill Climb/src/Controller/Controller_main/controller_main.js
+++ project Hill Climb/src/Controller/Controller_main/controller_main.js
@@ -16,6 +16,7 @@ export let settingGame1 = {
     let btnGameinMenu = document.getElementById("btnGameinMenu")
     btnGameinMenu.addEventListener("click", ()=>{
       btnMenuBoolean = true
+      console.log(btnMenuBoolean);
       location = "https://dj-rom.github.io/Hill_Climb/project%20Hill%20Climb/src/view/view_menu/menu.html"
     })
 
