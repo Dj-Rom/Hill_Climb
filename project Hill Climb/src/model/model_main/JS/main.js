b39d8a1bb97274c2b15e5d7e01b9842c233068f4
Aleh <alehkapusta@gmail.com>
2023-06-16 00:22:48 +0200

    Update main.js

--- project Hill Climb/src/model/model_main/JS/main.js
+++ project Hill Climb/src/model/model_main/JS/main.js
@@ -28,8 +28,8 @@ export let kontrole = {
 
 mobileKontrole();
 
-window.screen.lockOrientationUniversal = screen.lockOrientation || screen.mozLockOrientation || screen.msLockOrientation;
-if(window.screen.lockOrientationUniversal("portrait-secondary")){
+screen.lockOrientationUniversal = screen.lockOrientation || screen.mozLockOrientation || screen.msLockOrientation;
+if(screen.lockOrientationUniversal("portrait-secondary")){
   alert((" \u20D4    You need to rotate your device! ПЕРЕВЕРНИТЕ ВАШЕ УСТРОЙСТВО    \u20D5"))
 }
 function mobileKontrole() {
