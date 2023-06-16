ca1061fbafcbb0b30725ba0e06b608de23822466
Aleh <alehkapusta@gmail.com>
2023-06-16 18:09:52 +0200

    Update model_setting.js

--- project Hill Climb/src/model/model_setting/model_setting.js
+++ project Hill Climb/src/model/model_setting/model_setting.js
@@ -16,7 +16,7 @@ const settingBtn1 = document.getElementById('settingBtn')
     let link_was_clicked = false;
     document.addEventListener("click", function(e) {
     
-       if (e.target.nodeName.toLowerCase() === 'button') {
+       if (e.target.nodeName.toLowerCase() != 'button') {
           link_was_clicked = true;
        }
     }, true);
