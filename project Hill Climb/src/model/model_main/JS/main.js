d802b5e755c49cc113f239516b87e963a9240650
Aleh <alehkapusta@gmail.com>
2023-06-16 15:46:52 +0200

    Update main.js

--- project Hill Climb/src/model/model_main/JS/main.js
+++ project Hill Climb/src/model/model_main/JS/main.js
@@ -167,7 +167,7 @@ function game() {
     }
     if (grounded && playing) {
       this.rot -= (this.rot - angle) * 0.65;
-      this.rSpeed = this.rSpeed - (angle - this.rot);
+      this.rSpeed = this.rSpeed - (angle - this.rot)-100;
     }
     // balance the rotation when flying
     this.rSpeed += (kontrole.ArrowLeft - kontrole.ArrowRight) * 0.015;
@@ -206,7 +206,7 @@ function game() {
       x = x / 155;
       return lerp(perm[Math.floor(x)], perm[Math.ceil(x)], x - Math.floor(x));
     };
-earthFunc(cvs.height);
+    earthFunc(cvs.height);
 sound();
 gameTime();
 musicFonAndGaz();
