35dc4e22781a4f4e7117e2c89b252a2c99706354
Aleh <alehkapusta@gmail.com>
2023-06-16 10:48:05 +0200

    Update controller_main.js

--- project Hill Climb/src/Controller/Controller_main/controller_main.js
+++ project Hill Climb/src/Controller/Controller_main/controller_main.js
@@ -120,7 +120,7 @@ imgbr.addEventListener("touchend", eo => {
 
 let link_was_clicked = false;
 document.addEventListener("click", function(e) {
-   if (e.target.nodeName.toLowerCase() === 'a'||e.target.nodeName.toLowerCase() === 'button') {
+   if (e.target.nodeName.toLowerCase() === 'a'&& e.target.nodeName.toLowerCase() === 'button') {
       link_was_clicked = true;
    }
 }, true);
